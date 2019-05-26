2e4c0887ec4f9c7f35b3c5717b014d484d7e30b5
axemandev <axemandev@gmail.com>
2019-05-26 17:50:04 +0530

    Added Spring Lessons

---
+++ SpringLessonInCode/src/main/java/learn/axemandev/SpringLessonInCodeApplication.java
@@ -0,0 +1,13 @@
+package learn.axemandev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLessonInCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLessonInCodeApplication.class, args);
+	}
+
+}
